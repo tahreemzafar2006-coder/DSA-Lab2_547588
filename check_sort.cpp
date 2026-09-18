2110eafbb2ce37c3684e5bd5d7c0f3097106266c
tahreemzafar2006-coder <tahreemzafar2006@gmail.com>
2026-09-18 23:13:45 +0500

    Sorting Checking Function with Basic Test Cases

---
+++ check_sort.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+bool isSorted(const int* arr, const int size);
+
+void testSortedArray() {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == true);
+}
+
+void testUnsortedArray() {
+    int arr[] = {1, 4, 3, 5, 6};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == false);
+}
+
+void testDuplicateValues() {
+    int arr[] = {1, 2, 2, 3, 5};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == true);
+}
+
+void testSingleElement() {
+    int arr[] = {10};
+
+    bool result = isSorted(arr, 1);
+
+    assert(result == true);
+}
+
+void testDescendingArray() {
+    int arr[] = {5, 4, 3, 2, 1};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == false);
+}
+
+void testNegativeValues() {
+    int arr[] = {-5, -3, -1, 0, 2};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == true);
+}
+// My testCases 
+void testNullValues(){
+    int arr[]={};
+    bool result = isSorted(arr,0);
+    assert(result == true);
+
+}
+void  testunSortedNegativeValues(){
+    int arr[]={-1,-4,-1,-3,-6};
+    bool result = isSorted(arr,5);
+    assert(result == false);
+}
+
+int main() {
+
+    testSortedArray();
+    testUnsortedArray();
+    testDuplicateValues();
+    testSingleElement();
+    testDescendingArray();
+    testNegativeValues();
+    testNullValues();
+    testunSortedNegativeValues();
+
+    cout << "All tests passed!" << endl;
+
+    return 0;
+}
